30612a0bf9b97529336462db475d42dd99994ff0
Andrey <orion128@mail.ru>
2023-09-26 19:16:37 +0300

    TestService

--- src/main/java/MonthService.java
+++ src/main/java/MonthService.java
@@ -1,12 +1,8 @@
-public class Main {
-
-    public static void main(String[] args) {
-        MonthService service = new MonthService();
-        int expenses = 3000;
-        int income = 10000;
-        int treshhold = 20000;
+public class MonthService {
+    public int calculate(int income, int expenses, int treshhold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
+
         for (int month = 0; month < 12; month++) {
             if (money >= treshhold) {
                 count++; // увеличиваем счётчик месяцев отдыха
---
+++ src/test/java/MonthServiceTest.java
@@ -0,0 +1,6 @@
+public class MonthServiceTest {
+    public void test1(int expected, int income, int expenses, int threshold) {
+        MonthService service = new MonthService();
+
+    }
+}
